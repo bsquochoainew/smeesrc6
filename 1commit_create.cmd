0fd102d99f973fec3033f95a05df05cb8a38e956
drquochoai <drquochoai@gmail.com>
2015-04-03 08:02:15 +0700

    bsquochoainew

---
+++ 1commit_create.cmd
@@ -0,0 +1,9 @@
+git checkout -b master
+git add .
+git commit -am  "bsquochoainew"
+git push origin master
+git checkout -b bsquochoai
+git checkout bsquochoai
+git add .
+git commit -am  "bsquochoainew"
+git push origin bsquochoai
---
+++ 2commit_bsquochoai.cmd
@@ -0,0 +1,4 @@
+git checkout bsquochoai
+git add .
+git commit -am  "bsquochoainew"
+git push origin bsquochoai
